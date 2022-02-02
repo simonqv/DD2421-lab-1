51d279c3e056a7da1edb30be6b190fa9a9c9700d
Oxel40 <Oxel40@users.noreply.github.com>
2022-02-02 12:32:47 +0100

    code done?

--- dectrees/python/ass6.py
+++ dectrees/python/ass6.py
@@ -1,4 +1,6 @@
 import random
+from statistics import mean, variance
+import matplotlib.pyplot as plt
 
 import monkdata as m
 import dtree as d
@@ -40,27 +42,44 @@ def prune(tree, val_set):
         if d.check(best_pruned, val_set) > d.check(prev_best, val_set):
             prev_best = best_pruned
         else:
-            break
-    return prev_best
+            return prev_best
 
 
 if __name__ == '__main__':
+    iterations = 1000
     fractions = list(map(lambda i: i/10, range(3,9)))
-    scores = []
-    iterations = 100
-    # tree = t1
-    dataset = m.monk1
-    testset = m.monk1test
 
-    for frac in fractions:
-        scores.append([])
-        for i in range(iterations):
-            train, val = partition(dataset, frac)
-            tree = d.buildTree(train, m.attributes)
-            tree = prune(tree, val)
-            scores[-1].append(d.check(tree, testset))
+    for dataset, testset, tag in [(m.monk1, m.monk1test, 'monk1'), (m.monk3, m.monk3test, 'monk3')]:
+        scores = []
+        print(tag)
+        for frac in fractions:
+            scores.append([])
+            for i in range(iterations):
+                train, val = partition(dataset, frac)
+                tree = d.buildTree(train, m.attributes)
+                tree = prune(tree, val)
+                scores[-1].append(d.check(tree, testset))
 
-    print(scores)
-    print(list(map(lambda s: len(s), scores)))
-    print(fractions)
+        average = list(map(lambda s: mean(s), scores))
+        vari = list(map(lambda s: variance(s), scores))
+        for i in range(len(fractions)):
+            print(fractions[i], average[i], vari[i])
+        
+        plt.subplot(1, 2, 1)
+        plt.plot(fractions, average, '-o', label=tag)
+        plt.subplot(1, 2, 2)
+        plt.plot(fractions, vari, '-o', label=tag)
 
+
+    plt.subplot(1, 2, 2)
+    plt.legend()
+    plt.title('Average/Mean', size=30)
+    plt.xlabel('Fraction', size=20)
+    plt.ylabel('Score', size=20)
+    plt.subplot(1, 2, 1)
+    plt.legend()
+    plt.title('Variance', size=30)
+    plt.xlabel('Fraction', size=20)
+    plt.ylabel('Score', size=20)
+
+    plt.show()
